aee304d8bfa498c6059281a6d7b57b22cf584368
Michael Totschnig <michael@totschnig.org>
2020-05-13 10:25:51 +0200

    simplified

--- app/src/main/java/org/totschnig/robolectrictestapplication/TestUtils.kt
+++ app/src/main/java/org/totschnig/robolectrictestapplication/TestUtils.kt
@@ -5,13 +5,11 @@ import java.io.InputStream
 import java.io.InputStreamReader
 
 object TestUtils {
-    fun read(inputStream: InputStream): MutableList<String> {
-        val results = mutableListOf<String>()
+    fun read(inputStream: InputStream) = mutableListOf<String>().also { list ->
         inputStream.use { stream ->
             BufferedReader(InputStreamReader(stream)).use { reader ->
-                while (reader.readLine()?.also { results.add(it) } != null) {}
+                while (reader.readLine()?.also { list.add(it) } != null) {}
             }
         }
-        return results
     }
 }
